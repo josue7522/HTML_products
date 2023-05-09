70c9cdbffb511bd0bc3896c75f059bbff95ad22b
Josue Wendell <101228263+josue7522@users.noreply.github.com>
2023-05-08 21:36:34 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,69 @@
+<html>
+    <head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" /><title>Document</title>
+</head>
+<body>
+
+            </div>
+            <span class="facalogin">Olá! faça seu login ou cadastre-se</span>
+            <div class="barraverde"></div>
+            <span class="totaldemetalcoin">TOTAL METALCOINS</span>
+            <span class="produto-a-ser-trocado">PRODUTOS A SER TROCADOS</span>
+            <span class="home">Home</span>
+            <span class="o_que_metalcoins">O que é metalcoin?</span>
+            <span class="perfil">Perfil</span>
+            <span class="inicio_loja">INICIO / LOJA</span>
+            <div class="fundoverde_1"></div>
+            <span class="letratrocar_1">TROCAR</span>
+            <div class="fundoverde_2"></div>
+            <span class="letratrocar_2">TROCAR</span>
+            <div class="fundoverde_3"></div>
+            <span class="letratrocar_3">TROCAR</span>
+
+            <img id="chuteirola" src="img/folga.png" alt="">
+            <img id="chuteirola1" src="img/celular.png" alt="">
+            <img id="chuteirola2" src="img/valecompra.png" alt="">
+            <img id="chuteirola3" src="img/v4_11.png" alt="">
+            <img id="chuteirola4" src="img/valeviagem.png" alt="">
+
+
+
+            <input class="luzverde">
+            <span class="Todos">Todos</span>
+            <span class="coin1">1000metalcoins</span>
+            <span class="coin2">500metalcoins</span>
+            <span class="coin3">3000metalcoins</span>
+            <span class="coin4">4000metalcoins</span>
+            <span class="coin5">100metalcoins</span>
+
+
+
+
+            <span class="item1">SETE DIA DE FOLGA NA PROXIMA SEMANA</span>
+            <span class="item2">CELULAR SANSUNG GALAXY J8</span>
+            <span class="item3">VALE COMPRA NA LOJA DE 1000R$</span>
+            <span class="item4">VALE VIAGEM NO BRASIL</span>
+            <span class="item5">CHUTEIRA PROFISSIONAL</span>
+
+
+
+            <img id="logovix" src="img/v7_2.png" alt="">
+            <img id="logologin" src="img/v2_46.png" alt="">
+            <span class="logovix"></div>
+            <img id="lupa" src="img/v2_76.png" alt="">
+            
+
+
+
+            <div>
+                <input class="fundoverde_1" type="button" onclick="" value="TROCAR">
+                <input class="fundoverde_2" type="button" onclick="" value="TROCAR">
+                <input class="fundoverde_3" type="button" onclick="" value="TROCAR">
+                <input class="fundoverde_4" type="button" onclick="" value="TROCAR">
+                <input class="fundoverde_5" type="button" onclick="" value="TROCAR">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
